70c021bff7fff9764bcac9961aa573e82310c778
Diego Albitres <diego.albitres@gmail.com>
2022-10-05 18:37:53 -0500

    feat: initial creation of FindProductActivity

---
+++ app/src/main/java/com/dalbitresb/applistproduct/activities/FindProductActivity.kt
@@ -0,0 +1,12 @@
+package com.dalbitresb.applistproduct.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dalbitresb.applistproduct.R
+
+class FindProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_product)
+    }
+}
